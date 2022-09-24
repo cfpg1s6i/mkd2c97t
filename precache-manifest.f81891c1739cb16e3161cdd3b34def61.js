f3cb44db53d7e0fc7585c3b80a3ba5d0ec2bf3a1
x12amsu5 <114162395+x12amsu5@users.noreply.github.com>
2022-09-24 19:46:52 +0800

    Add files via upload

---
+++ precache-manifest.f81891c1739cb16e3161cdd3b34def61.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "3945dc336b1b1fb1eda6",
+    "url": "/css/app.4f4250f8.css"
+  },
+  {
+    "revision": "3945dc336b1b1fb1eda6",
+    "url": "/js/app.967dca40.js"
+  },
+  {
+    "revision": "c74bbbeb592be6abac56",
+    "url": "/css/chunk-00ee8072.f0d361d9.css"
+  },
+  {
+    "revision": "c74bbbeb592be6abac56",
+    "url": "/js/chunk-00ee8072.323bf661.js"
+  },
+  {
+    "revision": "378afa1dbce80c839086",
+    "url": "/js/chunk-0b046b52.d45ef4ab.js"
+  },
+  {
+    "revision": "5fd8a96679c90d591ece",
+    "url": "/css/chunk-22239e24.f0d361d9.css"
+  },
+  {
+    "revision": "5fd8a96679c90d591ece",
+    "url": "/js/chunk-22239e24.6912e8bc.js"
+  },
+  {
+    "revision": "1f2688bd1684060d9ca3",
+    "url": "/js/chunk-2d216257.df162628.js"
+  },
+  {
+    "revision": "1fd2a5dbf9d43aa9ab4a",
+    "url": "/js/chunk-2d238433.1735250e.js"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/css/chunk-2e73aee3.93c3732a.css"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/js/chunk-2e73aee3.664b36aa.js"
+  },
+  {
+    "revision": "56456bea2699831928b1",
+    "url": "/css/chunk-34d2c1c4.e2e10d2a.css"
+  },
+  {
+    "revision": "56456bea2699831928b1",
+    "url": "/js/chunk-34d2c1c4.f311d112.js"
+  },
+  {
+    "revision": "ef8ab7b0c386171338f7",
+    "url": "/css/chunk-5d3ae0d8.f0d361d9.css"
+  },
+  {
+    "revision": "ef8ab7b0c386171338f7",
+    "url": "/js/chunk-5d3ae0d8.43cafc0e.js"
+  },
+  {
+    "revision": "4858e9b24884bdbc95ee",
+    "url": "/js/chunk-a4189212.a6657841.js"
+  },
+  {
+    "revision": "def17a23f16d22bf9c49",
+    "url": "/css/chunk-a4b3ff6c.864c088c.css"
+  },
+  {
+    "revision": "def17a23f16d22bf9c49",
+    "url": "/js/chunk-a4b3ff6c.4ae212f6.js"
+  },
+  {
+    "revision": "d472166369a24cef85c0",
+    "url": "/js/element.7a4800b8.js"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "91295fa87df918411b49b7531da5d558",
+    "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/fonts/league-gothic.cd382dc8.woff"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/fonts/league-gothic.9900a464.eot"
+  },
+  {
+    "revision": "59bfb7d5ec18432e8bb9c662f42126f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "369f6129748a531567ab3535689be69a",
+    "url": "/site.webmanifest"
+  }
+];
